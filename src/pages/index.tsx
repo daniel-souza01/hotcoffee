6c50606da2bc859f56a6ff5b70075e6e04626060
Daniel Souza <82838453+DanielSouz4@users.noreply.github.com>
2022-02-07 19:29:57 -0300

    Estrutura do projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Coffee</title>
+      </Head>
+
+      <h1>Hello World</h1>
+    </>
+  )
+}
